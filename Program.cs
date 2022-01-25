2cf0ec9fc5a438124dac1b038be8c2394e741582
Andrew Birt <abirt1986@gmail.com>
2022-01-25 13:45:59 -0500

    put buildings in a list and iterate through list writing info about each building to the console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Planner
 {
@@ -6,6 +7,7 @@ namespace Planner
   {
     static void Main(string[] args)
     {
+      
       Building FiveOneTwoEigth = new Building("512 8th Avenue");
       FiveOneTwoEigth.Width = 100;
       FiveOneTwoEigth.Depth = 100;
@@ -13,14 +15,55 @@ namespace Planner
       FiveOneTwoEigth.Construct();
       FiveOneTwoEigth.Purchase("Bob Ross");
 
-      Console.WriteLine($@"
-        {FiveOneTwoEigth.GetAddress()}
-        ---------------
-        Designed By {FiveOneTwoEigth.GetDesigner()}
-        Constructed on {FiveOneTwoEigth.GetConstructionDate()}
-        Owned By {FiveOneTwoEigth.GetOwner()}
-        {FiveOneTwoEigth.Volume} cubic meters of space
-      ");
+      Building OneTwoThree = new Building("123 Main Street");
+      OneTwoThree.Width = 45;
+      OneTwoThree.Depth = 56;
+      OneTwoThree.Stories = 6;
+      OneTwoThree.Construct();
+      OneTwoThree.Purchase("Jim Bob");
+
+      Building FourFiveSix = new Building("456 Plain Drive");
+      FourFiveSix.Width = 65;
+      FourFiveSix.Depth = 76;
+      FourFiveSix.Stories = 7;
+      FourFiveSix.Construct();
+      FourFiveSix.Purchase("Sally Slappers");
+
+      Building SevenEightNine = new Building("789 Lucky Avenue");
+      SevenEightNine.Width = 77;
+      SevenEightNine.Depth = 45;
+      SevenEightNine.Stories = 5;
+      SevenEightNine.Construct();
+      SevenEightNine.Purchase("Kevin Smith");
+
+      Building FourTwoZero = new Building("420 High Place");
+      FourTwoZero.Width = 420;
+      FourTwoZero.Depth = 333;
+      FourTwoZero.Stories = 13;
+      FourTwoZero.Construct();
+      FourTwoZero.Purchase("Snoop Dogg");
+
+      List<Building> buildings = new List<Building>()
+      {
+        FiveOneTwoEigth,
+        OneTwoThree,
+        FourFiveSix,
+        SevenEightNine,
+        FourTwoZero
+      };
+
+      foreach (Building building in buildings)
+      {
+        Console.WriteLine($@"
+          {building.GetAddress()}
+          ---------------
+          Designed By {building.GetDesigner()}
+          Constructed on {building.GetConstructionDate()}
+          Owned By {building.GetOwner()}
+          {building.Volume} cubic meters of space
+        ");
+      }
+
     }
   }
 }
